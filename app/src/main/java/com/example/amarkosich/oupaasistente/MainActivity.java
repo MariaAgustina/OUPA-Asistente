6e4eefe3342b54b2f2bb16de3eae92f62f153da9
Mauro Capolupo <mauro.capolupo@mercadolibre.com>
2018-05-29 23:43:32 -0300

    firebase integration

--- app/src/main/java/com/example/amarkosich/oupaasistente/MainActivity.java
+++ app/src/main/java/com/example/amarkosich/oupaasistente/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.amarkosich.oupaasistente;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +43,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Log.i(getClass().getCanonicalName(), "Firebase token: " + FirebaseInstanceId.getInstance().getToken());
+
     }
 
     @Override
