dd2882d1790e8bf073e2daf0965807a4493cf743
Mauro Capolupo <mauro.capolupo@mercadolibre.com>
2018-06-09 17:23:02 -0300

    update styles

--- app/src/main/java/com/example/amarkosich/oupaasistente/activities/SOSAlarmActivity.java
+++ app/src/main/java/com/example/amarkosich/oupaasistente/activities/SOSAlarmActivity.java
@@ -4,7 +4,11 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.amarkosich.oupaasistente.R;
@@ -20,11 +24,56 @@ public class SOSAlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sosalarm);
 
         String alarmMessage = getIntent().getStringExtra("SOS_MESSAGE");
-        alarmText = findViewById(R.id.alarmMessageText);
-        alarmText.setText(alarmMessage);
+        if (alarmMessage != null) {
+            alarmText = findViewById(R.id.alarmMessageText);
+            alarmText.setText(alarmMessage);
+        }
 
         final MediaPlayer mp = MediaPlayer.create(this, R.raw.alarm);
         mp.start();
+
+        final ScaleAnimation growAnim = new ScaleAnimation(1.0f, 1.5f, 1.0f, 1.5f, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+        final ScaleAnimation shrinkAnim = new ScaleAnimation(1.5f, 1.0f, 1.5f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5F, Animation.RELATIVE_TO_SELF, 0.5F);
+
+        growAnim.setDuration(500);
+        shrinkAnim.setDuration(500);
+
+        final ImageView heartView = findViewById(R.id.heart_pulse_icon);
+
+        heartView.setAnimation(growAnim);
+        growAnim.start();
+
+        growAnim.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override
+            public void onAnimationStart(Animation animation){}
+
+            @Override
+            public void onAnimationRepeat(Animation animation){}
+
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                heartView.setAnimation(shrinkAnim);
+                shrinkAnim.start();
+            }
+        });
+        shrinkAnim.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override
+            public void onAnimationStart(Animation animation){}
+
+            @Override
+            public void onAnimationRepeat(Animation animation){}
+
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                heartView.setAnimation(growAnim);
+                growAnim.start();
+            }
+        });
+
 
         acceptButton = findViewById(R.id.aceptButton);
         acceptButton.setOnClickListener(new View.OnClickListener() {
