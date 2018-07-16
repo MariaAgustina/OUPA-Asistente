fe7663c0b7e46b129d2e8948a9728da7334bf364
Mauro Capolupo <mauro.capolupo@mercadolibre.com>
2018-07-15 22:39:13 -0300

    added finish

--- app/src/main/java/com/example/amarkosich/oupaasistente/activities/MainActivity.java
+++ app/src/main/java/com/example/amarkosich/oupaasistente/activities/MainActivity.java
@@ -132,6 +132,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.logout) {
             userSessionManager.logout();
+            finish();
             return true;
         }
 
